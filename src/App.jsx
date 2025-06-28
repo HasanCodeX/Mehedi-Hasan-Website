dbd057313c40d1184491e212c70d4d63918f23e6
Mehedi Hasan <dev.hasan10@gmail.com>
2025-06-28 08:58:29 +0600

    (feat) i add nav and hero

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import HeroSection from '../components/HeroSection';
+import SocialSidebar from '../components/SocialSidebar';
+import Navbar from '../components/Navbar';
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <Navbar />
+    <  HeroSection />
+   < SocialSidebar />
+
+    </>
+  )
+}
+
+export default App
