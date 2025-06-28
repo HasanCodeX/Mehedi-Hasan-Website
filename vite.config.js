927283bc9c8ea3318e14c29aa8d4190148c7affd
Mehedi Hasan <dev.hasan10@gmail.com>
2025-06-29 02:09:25 +0600

    (feat) i add vercel configure

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(),react()],
-// })
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    // For local dev (not needed for Vercel, but helps)
-    historyApiFallback: true,
-  }
+  plugins: [tailwindcss(),react()],
 })
