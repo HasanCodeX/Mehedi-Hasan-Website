54f0a24f7c9c53a7a9615b3a74fda023f4b05529
Mehedi Hasan <dev.hasan10@gmail.com>
2025-07-04 21:33:43 +0600

    (fix) i fix 404 pages routs

--- src/main.jsx
+++ src/main.jsx
@@ -1,62 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.jsx'
-import HeroSection from "../components/HeroSection";
-
-
-import AboutMe from "../components/AboutMe";
-import "./App.css";
-import ContactMe from '../components/ContactMe';
-import ProjectSection from "../components/ProjectSection";
-import NotFound from "../components/NotFound";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './App.css';
 
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom";
-import Layout from "../components/Layout";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Layout,
-    children: [
-      {
-        path: "/",
-        Component: HeroSection,
-      },
+} from 'react-router-dom';
 
-      {
-        path: "about",
-        Component: AboutMe,
-      },
-      {
-        path: "projects",
-        Component: ProjectSection,
-      },
-      {
-        path: "contact",
-        Component: ContactMe,
-      },
-      {
-        path: "*",
-        Component: NotFound,
-      }
-    ],
-  },
-]);
+import Layout from '../components/Layout';
+import HeroSection from '../components/HeroSection';
+import AboutMe from '../components/AboutMe';
+import ProjectSection from '../components/ProjectSection';
+import ContactMe from '../components/ContactMe';
+import NotFound from '../components/NotFound';
 
 import { HelmetProvider } from 'react-helmet-async';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <HeroSection /> },
+      { path: 'about', element: <AboutMe /> },
+      { path: 'projects', element: <ProjectSection /> },
+      { path: 'contact', element: <ContactMe /> },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <RouterProvider router={router} />
     </HelmetProvider>
-  </StrictMode>,
+  </StrictMode>
 );
-
-
-
-
-
